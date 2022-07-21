d70427a59995a81ba12054ddef852c6127251b2d
Stanislav <stason1986@mail.ru>
2022-07-21 23:10:03 +0300

    Second try to lesson11. Refactor Deadlock.

--- src/com/urase/webapp/Deadlock.java
+++ src/com/urase/webapp/Deadlock.java
@@ -5,34 +5,27 @@ public class Deadlock {
         Object lock1 = new Object();
         Object lock2 = new Object();
 
-        Thread thread1 = new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + " start");
-            synchronized (lock1) {
+        Thread thread1 = createThread(lock1, lock2);
+        Thread thread2 = createThread(lock2, lock1);
+        thread1.start();
+        thread2.start();
+    }
 
+    private static Thread createThread(Object object1, Object object2) {
+        return new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " start");
+            synchronized (object1) {
+                System.out.println("Run monitor on " + object1);
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
-                synchronized (lock2) {
-
+                synchronized (object2) {
+                    System.out.println("Run monitor on " + object2);
                 }
             }
             System.out.println(Thread.currentThread().getName() + " end");
-        }, "Thread1");
-
-        Thread thread2 = new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + " start");
-            synchronized (lock2) {
-                synchronized (lock1) {
-
-                }
-            }
-            System.out.println(Thread.currentThread().getName() + " end");
-        }, "Thread2");
-
-        thread1.start();
-        thread2.start();
+        });
     }
 }
