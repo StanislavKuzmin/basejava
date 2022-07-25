31b6e42a463207998eabb1b77de9b3943f396d37
Stanislav <stason1986@mail.ru>
2022-07-25 23:02:59 +0300

    Second try to lesson12. Refactor minValue in StreamApi.

--- src/com/urase/webapp/StreamApi.java
+++ src/com/urase/webapp/StreamApi.java
@@ -3,15 +3,11 @@ package com.urase.webapp;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class StreamApi {
 
     public int minValue(int[] values) {
-        int[] uniqueValues = Arrays.stream(values).distinct().sorted().toArray();
-        return IntStream.range(0, uniqueValues.length)
-                .map(i -> (int) (Math.pow(10, uniqueValues.length - 1 - i) * uniqueValues[i]))
-                .sum();
+        return Arrays.stream(values).distinct().sorted().reduce((l, r) -> l * 10 + r).getAsInt();
     }
 
     public List<Integer> oddOrEven(List<Integer> integers) {
@@ -23,8 +19,8 @@ public class StreamApi {
 
     public static void main(String[] args) {
         StreamApi streamApi = new StreamApi();
-        int[] array1 = {1,2,3,3,2,3};
-        int[] array2 = {9,8};
+        int[] array1 = {1, 2, 3, 3, 2, 3};
+        int[] array2 = {9, 8};
         System.out.println(streamApi.minValue(array1));
         System.out.println(streamApi.minValue(array2));
         System.out.println(streamApi.oddOrEven(Arrays.stream(array1).boxed().collect(Collectors.toList())));
